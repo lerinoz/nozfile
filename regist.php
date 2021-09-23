8a7d43a37de86fc976c4a9435b38c57feb9b0fa1
lerinoz <91242519+lerinoz@users.noreply.github.com>
2021-09-23 12:09:15 +0400

    Add files via upload

---
+++ regist.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html1>
+<head>
+<label for="name"><b>name</b></label>
+<input type="text" placeholder="Please enter your name" name="name" required>
+
+<label for="last name"><b>last name</b></label>
+<input type="text" placeholder="Please enter your last name" name="last name" required>
+
+<label for="email"><b>Email</b></label>
+<input type="text" placeholder="Enter your mail" name="email" required>
+
+<label for="psw"><b>password</b></label>
+<input type="password" placeholder="enter password" name="psw" required>
+
+<label for="psw-repeat"><b>Repeat password</b></label>
+<input type="password" placeholder="Repeat password" name="psw-repeat" required>
+
+<input type="submit" name="" value="login">
+
+</head>
+    <meta charset="utf-8" />
+<body>
+   <p>გამარჯობა საქართველო ჰელლო ვორლდ</p>
+   <p>ეს საიტი განკუთვნილია ყველაფრისთვის</p>
+<!--  aq aris saiti gankutvnili xz vistvis -->    
+    <p>es teqtsi gamoiyofaa </p>
+	<p><h1>სათაური N-1</h1> </p>
+	<p><h2>satauri n2</2></p>
+	<p> <b>sasurveli teqsi gadidebuli formatsi</b> </p>
+	<p> <strong>sasurveli teqsi meore variani gamsxvilebuli</strong> </p>
+	<p> <i>es teqsti gamoisaxme italikis formit</i> </p>
+	<p> <em>es teqsti aris meore varianti raa</em> </p>
+	<p> <strike>es teqsti iqnebagamosaxuli gadasmuli xazit</strike> </p>
+	<p> <u>es teqsti gamoisaxeba qmeova xazgsmaukl</u> </p>
+	<p> <abbr title="es aris HTML saprogramo ena">HTML</abrr> </p>
+	<p> <address>georgia, tbilisi</address> </p>
+-</ol>   
+<img src="https://i.pinimg.com/originals/4d/2c/26/4d2c26f13b9b8c73f40e00dd8f8eb8ea.jpg" width="20%" height="14%" alt="mercdes amg" border="0px" />
+
+</body>
+</html1>
